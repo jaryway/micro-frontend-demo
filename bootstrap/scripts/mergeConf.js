df5f1205b59e2fec17fb2810545a016d57cf0a7b
zxwei <2971652817@qq.com>
2019-09-24 19:19:05 +0800

    rename file

--- bootstrap/scripts/mergeConf.js
+++ bootstrap/scripts/mergeConf.js
@@ -3,11 +3,11 @@ const path = require('path');
 // 找到以 -app 结尾的文件夹
 
 // 提取里面的 project.json，并写入 project.config.json 中
-console.log('__dirname', __dirname);
+// console.log('__dirname', __dirname);
 const rootPath = path.join(__dirname, '..', 'public');
 fs.readdir(rootPath, function(err, files) {
   if (err) throw err;
-  console.log('files', files);
+  // console.log('files', files);
 
   const appDirList = files
     .map(m => path.join(rootPath, m))
@@ -21,14 +21,12 @@ fs.readdir(rootPath, function(err, files) {
     return JSON.parse(fs.readFileSync(filename, { encoding: 'utf8' }));
   });
   const configFilename = path.join(rootPath, 'project.config.json');
-  let configContent = fs.readFileSync(configFilename, {
-    encoding: 'utf8',
-  });
-  let config = { ...JSON.parse(configContent), projects };
+  const configStr = fs.readFileSync(configFilename, { encoding: 'utf8' });
+  const config = { ...JSON.parse(configStr), projects };
 
   fs.writeFile(configFilename, JSON.stringify(config), err => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
-  console.log('constentList', JSON.stringify(config));
+  // console.log('constentList', JSON.stringify(config));
 });
